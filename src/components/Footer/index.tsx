5e5076c26a6609c5b6bf7381ab236d5354af7687
Dante Benicio <dantecosta79@gmail.com>
2022-09-07 23:56:44 -0300

    Footer component created

---
+++ src/components/Footer/index.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Footer() {
+  return (
+    <footer className='w-full text-center text-gray font-inter mt-20 p-2'>
+      <p>Todos os direitos reservados aos criadores e produtores dos animes</p>
+    </footer>
+  )
+}
