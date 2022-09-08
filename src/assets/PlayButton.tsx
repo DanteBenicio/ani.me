88f57f5c7b89ae57fafa57b39ed5ad2867401d9d
Dante Benicio <dantecosta79@gmail.com>
2022-09-07 23:57:18 -0300

    assets folder

---
+++ src/assets/PlayButton.tsx
@@ -0,0 +1,8 @@
+export default function PlayButton() {
+  return (
+    <svg width="97" height="62" viewBox="0 0 97 62" fill="none" xmlns="http://www.w3.org/2000/svg" className="absolute translate-x-[-50%] translate-y-[-50%] left-2/4 top-2/4 hover:brightness-105 transition-all cursor-pointer">
+      <rect width="97" height="62" rx="16" fill="#00A3FF"/>
+      <path d="M66 30.5L39.75 45.6554V15.3446L66 30.5Z" fill="white"/>
+    </svg>
+  )
+}
