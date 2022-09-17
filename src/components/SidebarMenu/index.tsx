792be2f132e575b6e38a9e44cb01634ddb22c23a
Dante Benicio <dantecosta79@gmail.com>
2022-09-16 23:29:23 -0300

    SidebarMenu updated

--- src/components/SidebarMenu/index.tsx
+++ src/components/SidebarMenu/index.tsx
@@ -1,7 +1,31 @@
 import React from 'react'
+import useClickOutside from '../../hooks/useClickOutside'
+import Burger from '../Burger'
+import styles from './styles.module.css'
 
-export default function SidebarMenu() {
+interface SidebarMenuProps {
+  handleToggleMenu: () => void
+  showSidebarMenu: boolean
+}
+
+export default function SidebarMenu({handleToggleMenu, showSidebarMenu}: SidebarMenuProps) {
+
+  useClickOutside(handleToggleMenu)
+
   return (
-    <div>SidebarMenu</div>
+    <div className={styles.sidebar_container} >
+      <aside className={`${styles.sidebar} ${showSidebarMenu ? styles.active : ''}`}>
+        <div className="p-4">
+          <Burger showSidebarMenu={showSidebarMenu} handleToggleMenu={handleToggleMenu}/>
+        </div>
+
+        <ul className="flex items-center flex-col mt-4 justify-center gap-4">
+          <li className={styles.li}>Início</li>
+          <li className={styles.li}>Lista</li>
+          <li className={styles.li}>Gêneros</li>
+          <li className={styles.li}>Novos Episódios</li>
+        </ul>
+      </aside>
+    </div>
   )
 }
