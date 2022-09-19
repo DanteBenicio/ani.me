b89c2c7a6a220f2ad971eed2169ccf600f4a98b8
Dante Benicio <dantecosta79@gmail.com>
2022-09-18 23:40:15 -0300

    refactored code

--- src/utils/formatAnimeName.ts
+++ src/utils/formatAnimeName.ts
@@ -1,3 +1,3 @@
 export default function formatAnimeName(text: string) {
-  return text.replace(/(\/anime\/)/gi, '').replace(/%20/g, ' ').replace(/%C3%97/gi, '×').replace(/%E2%98%86/gi, '☆')
+  return text.replace(/(\/anime\/)/gi, '')
 }
