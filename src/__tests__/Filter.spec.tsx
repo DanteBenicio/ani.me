61756f1064973a24f0506670684fa118206f0418
Dante Benicio <dantecosta79@gmail.com>
2022-09-26 23:13:23 -0300

    added 2 test cases

--- src/__tests__/Filter.spec.tsx
+++ src/__tests__/Filter.spec.tsx
@@ -4,7 +4,7 @@ import Filter from "../components/Filter"
 
 describe("Filter component", () => {
   it("should render 27 items", () => {
-    const { container, debug } = render(<Filter setSelectedFilter={() => {}} />)
+    const { container } = render(<Filter setSelectedFilter={() => {}} />)
 
     const elements = Array.from(container.firstElementChild?.children!)
 
@@ -12,7 +12,7 @@ describe("Filter component", () => {
   })
 
   it("should added active class for the clicked button", () => {
-    const { container, debug } = render(<Filter setSelectedFilter={() => {}} />)
+    const { container } = render(<Filter setSelectedFilter={() => {}} />)
 
     const elements = Array.from(container.firstElementChild?.children!)
     const firstButton = elements[0]
@@ -25,8 +25,33 @@ describe("Filter component", () => {
 
     fireEvent.click(firstButton)
 
-    debug(firstButton)
-    
     expect(firstButton).toHaveClass('active')
+  })
+
+  it('should have remove class if the clicked button already has active class', async () => {
+    const { container } = render(<Filter setSelectedFilter={() => {}} />)
+
+    const elements = Array.from(container.firstElementChild?.children!)
+    const firstButton = elements[0]
+    const secondButton = elements[1]
+
+    firstButton.classList.add('active')
+
+    await userEvent.click(secondButton)
+
+    expect(firstButton.classList).toHaveLength(1)
+  })
+
+  it('should ignore the removal of the clicked button class', async () => {
+    const { container } = render(<Filter setSelectedFilter={() => {}} />)
+
+    const elements = Array.from(container.firstElementChild?.children!)
+    const firstButton = elements[0]
+
+    firstButton.classList.add('active')
+
+    await userEvent.click(firstButton)
+
+    expect(firstButton.classList).toHaveLength(2)
   })
 })
